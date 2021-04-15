62b85edaf15fc7b133a572a4330a385409a7bf36
root <root@LAPTOP-BFNOOAN1.localdomain>
2021-04-15 07:28:23 +0200

    deleted:    .autotools 	deleted:    .cproject 	deleted:    .project 	deleted:    7889.txt 	deleted:    8113.txt 	deleted:    HistoryStatisticsCounters-1.txt 	deleted:    HistoryStatisticsCounters.csv 	deleted:    HistoryStatisticsCounters.txt 	deleted:    a.out 	deleted:    cdrom 	deleted:    cdrom.c 	deleted:    config.xml 	deleted:    config.xsd 	deleted:    disk-usage.cpp 	deleted:    dtmf_gen.xml 	deleted:    dtmf_gen.xsd 	deleted:    dynamic.xml 	deleted:    dynamic.xsd 	deleted:    error.txt 	deleted:    gen 	deleted:    gen.c 	deleted:    getenv.txt 	deleted:    gzip 	deleted:    history.txt 	deleted:    job.txt 	deleted:    ls.txt 	deleted:    make.txt 	deleted:    man.txt 	deleted:    memcln 	deleted:    memcln-1 	deleted:    memsrv 	deleted:    memsrv-1 	deleted:    memwrk 	deleted:    memwrk-1 	deleted:    mounts-root.txt 	deleted:    mounts-rvod.txt 	deleted:    mounts.txt 	deleted:    mq-triggers.xml 	deleted:    mqtrigger.xml 	deleted:    ms1010ax.xsd 	deleted:    ms_options.xml 	deleted:    ms_options.xsd 	deleted:    namespace.txt 	deleted:    nodeset.txt 	deleted:    pardata.bin 	deleted:    ps.sh 	deleted:    sdl-triggers.xml 	deleted:    sdltrigger.xml 	deleted:    select.txt 	deleted:    stat_adapter.xml 	deleted:    stat_adapter.xsd 	deleted:    sys_interface.sh 	deleted:    system.columns.xml 	deleted:    system.tables.xml 	deleted:    system.tables.xsd 	deleted:    system.xml 	deleted:    system.xsd 	deleted:    tone.xml 	deleted:    tone.xsd 	deleted:    trigger.xml 	deleted:    update-stmt.txt 	deleted:    xdbd-ssh.txt~ 	deleted:    xdbd-start.sh 	deleted:    xdbd-test.sh 	deleted:    xdbd-trigger.xml 	deleted:    xdbd.sh 	deleted:    xdbd_trigger.xsd 	deleted:    xdbdmgr.log 	deleted:    xml-size-test 	deleted:    xml-size-test.cpp 	deleted:    xxx.c

--- sys_interface.sh
+++
@@ -1,4 +0,0 @@
-xdbd-test "SELECT sys_intf_type, mn_intf_id, used_lm FROM sys_interface WHERE ((nodeid = ?) AND (sys_intf_id = ?))" 2000 130 >/dev/null 2>&1&
-xdbd-test "DELETE FROM sys_interface WHERE nodeid = ? AND sys_intf_id = ?" 2000 130 >/dev/null 2>&1&
-xdbd-test "INSERT INTO sys_interface (nodeid, sys_intf_id, sys_intf_type, mn_intf_id, used_lm) VALUES (?, ?, ?, ?, ?)" 2000 130 14 0 0 >/dev/null 2>>sys_interface.log&
-xdbd-test "UPDATE sys_interface SET sys_intf_type = ?, mn_intf_id = ?, used_lm = ? WHERE ((nodeid = ?) AND (sys_intf_id = ?))" 15 0 0 2000 130 >/dev/null 2>&1&
